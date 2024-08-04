a55433cf848dff7442dbef889cc030dde3dd84d2
YeoSungeun <dutjddms@naver.com>
2024-08-04 23:02:42 +0900

    [Password] PasswordViewController 구현

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let basicColor = BehaviorSubject(value: UIColor.systemRed)
+    var discriptionLabel = UILabel()
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +25,43 @@ class PasswordViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
+        
     }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+    func bind() {
+        let validation = passwordTextField.rx.text.orEmpty
+            .map { $0.count >= 8 }
+        
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        validation
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? UIColor.systemGreen : UIColor.systemRed
+                owner.basicColor.onNext(color)
+//                owner.discriptionLabel.rx.isHidden = !value
+                owner.discriptionLabel.rx.text.onNext("8자리 이상 입력하세요")
+            }
+            .disposed(by: disposeBag)
+        basicColor //Observable.just(UIColor.systemGreen)로 보낼게~
+            .bind(to:
+                nextButton.rx.backgroundColor,
+                passwordTextField.rx.textColor,
+                passwordTextField.rx.tintColor
+            ) //nextButton.rx.어쩌구 -> rx특성으로 바꾸는 과정
+            .disposed(by: disposeBag)
+        
+        // borderColor 는 cgColor이기 때문에 따로 바인딩 해줘야함 ~?!
+        basicColor
+            .map { $0.cgColor }
+            .bind(to: passwordTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
     func configureLayout() {
